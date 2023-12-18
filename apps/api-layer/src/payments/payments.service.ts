ef7cfe6f5d38043b3b8b85cbe80c9cdc6db61cb4
Andrea Caldera <andrea.caldera@gmail.com>
2023-12-18 10:28:20 +0000

    fix

--- apps/api-layer/src/payments/payments.service.ts
+++ apps/api-layer/src/payments/payments.service.ts
@@ -1,13 +1,9 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { Injectable } from '@nestjs/common';
 import type { Payment } from '@recharge/payment';
 
 @Injectable()
 export class PaymentsService {
   private payments = new Map<string, Payment>();
-  constructor(
-    @Inject('API_GATEWAY_PAYMENTS') public paymentsProxy: ClientProxy,
-  ) {}
 
   async findPaymentById(id: string) {
     return await Promise.resolve(this.payments.get(id));
